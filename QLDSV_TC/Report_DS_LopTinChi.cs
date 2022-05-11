e19517a05eac8e0c7782881ca8542ef0bb4fa748
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2022-05-11 10:24:34 +0700

    add report ds LTC

---
+++ QLDSV_TC/Report_DS_LopTinChi.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace QLDSV_TC
+{
+    public partial class Report_DS_LopTinChi : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Report_DS_LopTinChi()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
